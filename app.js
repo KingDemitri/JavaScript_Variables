e45a8f648e3cd7f420b9a0b9436b52f12d5331b1
Demitri Cameron <oblivion9794@gmail.com>
2023-01-09 12:23:03 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,48 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Demitri"
+let lastName = "C."
+var age = 28
 
 // Exercise 2
+string = firstName
+string = lastName
+number = age
 
 // Exercise 3
+let language = "Javascript"
+let createdYear = 1995
+let isCaseSensitive = true
+
+// Exercise 4
+let price = 19.99
+let isOnSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+
+// Exercise 5
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(createdYear)
+console.log(isCaseSensitive)
+console.log(price)
+console.log(isOnSale)
+console.log(salePercentage)
+console.log(stock)
+console.log(inStock)
+console.log(selectedSize)
+console.log(title)
+console.log(author)
+console.log(pageCount)
+console.log(bookmark)
+console.log(hasRead)
